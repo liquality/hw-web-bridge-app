fc72c7d514a7be15ba324376ccae40d4f2293fe7
Bradley <bradleysuira@gmail.com>
2021-10-28 16:45:18 -0500

    fix: fixed public path in config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const version = require('./package.json').version
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? `/dist/hw-web-bridge-app/${version}` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `./dist/${version}` : '/',
   outputDir: `dist/${version}`
 }
